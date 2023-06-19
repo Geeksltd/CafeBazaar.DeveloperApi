c66b091074527fe8f4567b9e2d4382f85d3995a4
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2023-06-19 15:38:22 +0330

    Fix prop names

--- Requests/CafeBazaarCancelSubscriptionRequest.cs
+++ Requests/CafeBazaarCancelSubscriptionRequest.cs
@@ -10,19 +10,19 @@
         /// <summary>
         /// Package name of the app in which product of the request subscription is defined.
         /// </summary>
-        [JsonPropertyName("grant_type")]
+        [JsonPropertyName("package_name")]
         public string PackageName { get; set; }
 
         /// <summary>
         /// SKU of the subscription product.
         /// </summary>
-        [JsonPropertyName("grant_type")]
+        [JsonPropertyName("subscription_id")]
         public string SubscriptionId { get; set; }
 
         /// <summary>
         /// Purchase token returned by Bazaar to app when subscription started. You can also use the token of any of the further billings of that subscription.
         /// </summary>
-        [JsonPropertyName("grant_type")]
+        [JsonPropertyName("purchase_token")]
         public string PurchaseToken { get; set; }
 
         public Task Validate()
